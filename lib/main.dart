cf619e7f003b416e81b780e3278c2c288edbab1d
mt00145 <mabutareki00145@gmail.com>
2025-05-21 15:35:34 -0400

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var _counter = 0.0;
+  var _myFontSize = 30.0;
+
+  void setNewValue(double value) {
+    setState((){
+      _counter = value;
+      _myFontSize = value;
+    });
+  }
+
+  void _incrementCounter() {
+    setState(() {
+      if(_counter < 99) {
+        _counter++;
+        _myFontSize = _counter;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('You have pushed the button this many times:', style: TextStyle(fontSize: _myFontSize),),
+            Text(
+              '$_counter',
+              style: TextStyle(fontSize: _myFontSize),
+            ),
+
+            Slider(value:_counter, max:100.0, onChanged: setNewValue, min:0.0,)
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Incremen  t',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
